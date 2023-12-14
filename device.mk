5aa9ab2175378e2135f0e2baee7e7b70776c7e95
mhmdeve <mhmdeveloperofficial@gmail.com>
2023-12-14 06:30:43 +0530

    ginkgo: Add ViPER4Android FX support
    * Need to clone this:
    git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -448,7 +448,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
-# Wifi
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
