e132f72f674794e9b4e99405c7adde033d0bb7a3
Daniel Hk <ifloraxd@gmail.com>
2024-02-27 22:44:32 +0000

    ginkgo: Arrow Bringup

--- arrow_ginkgo.mk
+++ arrow_ginkgo.mk
@@ -35,4 +35,4 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-DEVICE_MAINTAINER := Adithya (ghostrider_reborn)
+DEVICE_MAINTAINER := Hirokixd28
